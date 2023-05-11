986a785f66868836e4dd2cc920a1be6acfec2a44
javierpantoja1998 <cientificodebanco@gmail.com>
2023-05-11 13:01:52 +0200

    Contexts hechos

--- ApiTiendaZapatillasJPL/Data/ZapatillasContext.cs
+++ ApiTiendaZapatillasJPL/Data/ZapatillasContext.cs
@@ -1,6 +1,16 @@
-﻿namespace ApiTiendaZapatillasJPL.Data
+﻿using Microsoft.EntityFrameworkCore;
+using NuggetTiendaZapatillasJPL.Models;
+
+namespace ApiTiendaZapatillasJPL.Data
 {
-    public class ZapatillasContext
+    public class ZapatillasContext : DbContext
     {
+        public ZapatillasContext(DbContextOptions<ZapatillasContext> options)
+           : base(options) { }
+
+        public DbSet<Zapatilla> Personajes { get; set; }
+        public DbSet<VistaZapatillasCategoria> ZapatillasCategoria { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<Compra> Compras { get; set; }
     }
 }
