cc0179bda55221c322196fd8645e6a73f2e17bc6
javierpantoja1998 <cientificodebanco@gmail.com>
2023-05-11 14:05:06 +0200

    pegandome con el controller

---
+++ ApiTiendaZapatillasJPL/Controllers/UsuariosController.cs
@@ -0,0 +1,37 @@
+﻿using ApiTiendaZapatillasJPL.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NuggetTiendaZapatillasJPL.Models;
+
+namespace ApiTiendaZapatillasJPL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private RepositoryUsuarios repo;
+
+        public UsuariosController(RepositoryUsuarios repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet("{id}")]
+        
+        public async Task<ActionResult<Usuario>> FindUsuario(int id)
+        {
+            return await this.repo.FindUsuarioAsync(id);
+        }
+
+        //METODO PARA INSERTAR USUARIO
+        [HttpPost]
+        public async Task<ActionResult> InsertUsuario(Usuario user)
+        {
+            //await this.repo.InsertUsuario(user.IdUsuario, user.Nombre,
+            //    user.Dni, user.Direccion, user.Telefono, user.Email,
+            //    user.Password);
+            //return Ok();
+        }
+    }
+}
