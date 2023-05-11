436b68777478cc808ac21acea7d1fdca70429357
javierpantoja1998 <cientificodebanco@gmail.com>
2023-05-11 13:36:22 +0200

    controller de zzapatillas creado

---
+++ ApiTiendaZapatillasJPL/Controllers/ZapatillasController.cs
@@ -0,0 +1,32 @@
+﻿using ApiTiendaZapatillasJPL.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NuggetTiendaZapatillasJPL.Models;
+
+namespace ApiTiendaZapatillasJPL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ZapatillasController : ControllerBase
+    {
+        private RepositoryZapatillas repo;
+
+        ZapatillasController(RepositoryZapatillas repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Zapatilla>>> Get()
+        {
+            return await this.repo.GetZapatillassAsync();
+        }
+
+        //FUNCION PARA SACAR LOS DETALLES
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Zapatilla>> FindZapatilla(int id)
+        {
+            return await this.repo.FindZapatillaAsync(id);
+        }
+    }
+}
